dff5c506069843014c5f404b93a2b34ba30bb7a6
sereyvathna21 <vattdy192@gmail.com>
2025-11-08 23:56:47 +0700

    Deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, CI/CD!");
+  res.send("Hello, GitHub Actions!");
 });
 
 export default app;
